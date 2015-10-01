f9a45093e50f47d0d329b5a388669344bd5410b6
supaheckafresh <neilstrain@gmail.com>
2015-09-30 20:12:46 -0700

    Add logNums.js

--- logNums.js
+++ logNums.js
@@ -1,5 +1,31 @@
+
+/* while */
 var x = 1;
 while(x < 11){
-  console.log(x);
+  //console.log(x);
   x++;
+}
+
+/* do-while */
+var x = 1;
+do {
+  //console.log(x);
+  x++;
+} while (x < 11);
+
+
+for (var x = 0; x < 11; x++) {
+  if (x !== 5 && x !== 7) {
+    //console.log(x);
+  }
+}
+
+for (var x = 0; x < 11; x++) {
+  //(x !== 5 && x !== 7) ? console.log(x) : false;
+}
+
+for (var x = 1; x < 11; x++) {
+  if (x === 7)
+    break;
+  console.log(x);
 }
